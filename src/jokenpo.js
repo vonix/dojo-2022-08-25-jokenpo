7418c466f2fd0990f30613a661fb127b30a38865
Luiz Amorim <luiz.simples@gmail.com>
2022-08-25 11:13:39 -0400

    teste para simular empate entre 3 jogadores

--- src/jokenpo.js
+++ src/jokenpo.js
@@ -1,8 +1,18 @@
 const elements = ['tesoura', 'papel', 'pedra']
 
-export default (player1, player2) => {
-  if (player1 === player2) return 'Empate'
+export default (players) => {
 
+  let winner = players.shift()
+  
+  for (const player of players) {
+    winner = resultGame(winner, player)
+  }
+  
+  return winner
+}
+
+const resultGame = (player1, player2) => {
+  if (player1 === player2) return 'Empate'
   const choice1Index = elements.indexOf(player1)
   const choice2Index = elements.indexOf(player2)
   const diff = choice1Index - choice2Index
--- tests/src/jokenpo.test.js
+++ tests/src/jokenpo.test.js
@@ -3,41 +3,62 @@ import jokenpo from '../../src/jokenpo'
 test('pedra ganha de tesoura', () => {
   const playerVitinho = 'pedra'
   const playerFran = 'tesoura'
-  expect(jokenpo(playerVitinho, playerFran)).toBe(playerVitinho)
+  expect(jokenpo([playerVitinho, playerFran])).toBe(playerVitinho)
 });
 
 test('Tesoura ganha de Papel', () => {
   const playerVitinho = 'papel'
   const playerFran = 'tesoura'
-  expect(jokenpo(playerVitinho, playerFran)).toBe(playerFran)
+  expect(jokenpo([playerVitinho, playerFran])).toBe(playerFran)
 });
 
 test('Papel ganha de Pedra', () => {
   const playerVitinho = 'papel'
   const playerFran = 'pedra'
-  expect(jokenpo(playerVitinho, playerFran)).toBe(playerVitinho)
+  expect(jokenpo([playerVitinho, playerFran])).toBe(playerVitinho)
 });
 
 test('Papel ganha de Pedra', () => {
   const playerVitinho = 'pedra'
   const playerFran = 'papel'
-  expect(jokenpo(playerVitinho, playerFran)).toBe(playerFran)
+  expect(jokenpo([playerVitinho, playerFran])).toBe(playerFran)
 });
 
 test('tesoura perde para pedra', () => {
   const playerVitinho = 'tesoura'
   const playerFran = 'pedra'
-  expect(jokenpo(playerVitinho, playerFran)).toBe(playerFran)
+  expect(jokenpo([playerVitinho, playerFran])).toBe(playerFran)
 });
 
 test('Pedra perde para Papel', () => {
   const playerVitinho = 'papel'
   const playerFran = 'pedra'
-  expect(jokenpo(playerVitinho, playerFran)).toBe(playerVitinho)
+  expect(jokenpo([playerVitinho, playerFran])).toBe(playerVitinho)
 });
 
 test('Pedra empata com pedra', () => {
   const playerVitinho = 'pedra'
   const playerFran = 'pedra'
-  expect(jokenpo(playerVitinho, playerFran)).toBe('Empate')
+  expect(jokenpo([playerVitinho, playerFran])).toBe('Empate')
+});
+
+test('Estao inventando moda', () => {
+  const playerVitinho = 'agulha'
+  const playerFran = 'agulha'
+  expect(jokenpo([playerVitinho, playerFran])).toBe('Empate')
+});
+
+
+test('tres malucos no pedaço', () => {
+  const playerVitinho = 'tesoura'
+  const playerFran = 'tesoura'
+  const playerAntonio = 'pedra'
+  expect(jokenpo([playerVitinho, playerFran, playerAntonio])).toBe(playerAntonio)
+});
+
+test('tesoura e tesoura deve retornar pedra', () => {
+  const playerVitinho = 'tesoura'
+  const playerFran = 'papel'
+  const playerAntonio = 'pedra'
+  expect(jokenpo([playerVitinho, playerFran, playerAntonio])).toBe('Empate')
 });
